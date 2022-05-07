9b375176a532016938285d4168d247e859c37b71
John Snider <j.snider.8101220@gmail.com>
2022-05-07 11:51:32 -0400

    java practice age in dog years

---
+++ javaScriptPractice/age-to-dogyears.js
@@ -0,0 +1,24 @@
+// Variable for age
+const myAge = 47;
+
+// Variable that will change
+let earlyYears = 2;
+earlyYears *= 10.5;
+// Creating variable that subtract 2 from myAge saving to varialbe to laterYears.
+let laterYears = myAge - 2;
+// Multipling laterYears variable by 4.
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+// Adding earlyYears and laterYears and saving in variable named myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'John Snider'.toLowerCase();
+// A statement that displays your name and age in dog years.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
+
+
+
