964c510185a93b57cc6ab837ebb9d0518bc24b9c
John Snider <j.snider.8101220@gmail.com>
2022-05-07 11:42:34 -0400

    prs product fix

--- angular/prs/src/app/features/product-edit/product-edit.component.ts
+++ angular/prs/src/app/features/product-edit/product-edit.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
+import { Vendor } from 'src/app/models/vendor.model';
 import { ProductService } from 'src/app/Services/product.service';
+import { VendorService } from 'src/app/Services/vendor.service';
+
 
 @Component({
   selector: 'app-product-edit',
@@ -12,10 +15,21 @@ export class ProductEditComponent implements OnInit {
 
   product: Product = new Product()
   productId: number = 0
+  vendorId: number = 0
+  vendors: Vendor[] = []
 
-  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router, private vendorService: VendorService) { }
 
   ngOnInit(): void {
+
+    this.vendorService.getAll().subscribe(data => {
+      console.log(data)
+      this.vendors = data
+    },
+      error => { console.log(error) }
+    )
+
+    //This is getting the product, working code
     this.route.params.subscribe(
       params => {
         this.productId = params.id
@@ -23,6 +37,7 @@ export class ProductEditComponent implements OnInit {
           data => {
             if (data.length > 0) {
               this.product = data[0]
+
             }
             console.log(data)
 
@@ -45,5 +60,10 @@ export class ProductEditComponent implements OnInit {
 
     )
   }
+  compareFn(vendor1: Vendor, vendor2: Vendor) {
+    return vendor1.id === vendor2.id
+
+  }
+
 
 }
